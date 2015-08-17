d410a1097f0b600cf754c13f0cd8e356e2dce1fc
Oliver Mooney <oliver.mooney@gmail.com>
2015-08-17 18:12:50 +0100

    CLI integration tests - made some function definitions public instead of private to extend reach of tests (core.clj) - mocked out exit fn to allow testing (the JVM exits in normal operation) (core_test.clj) - tested options parsing using regular expressions to catch features of the expected text - tested exit codes directly - one test to catch supplied filenames is failing due to missing implementation in core.clj

--- test/lonelyplanet/core_test.clj
+++ test/lonelyplanet/core_test.clj
@@ -1,7 +1,33 @@
 (ns lonelyplanet.core-test
   (:require [clojure.test :refer :all]
-            [lonelyplanet.core :refer :all]))
+            [lonelyplanet.core :refer :all]
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+            [clojure.tools.cli :refer [parse-opts]]
+            [clojure.string :as s]
+            [aprint.core :refer (aprint)]))
+
+(defn cli-test-fixture
+  [f]
+  "Redefine the exit fn so it doesn't quit the JVM on completion, to allow testing."
+  (with-redefs
+       [exit (fn [status msg] {:status status :msg msg})]
+       (try (f))))
+
+(use-fixtures :once cli-test-fixture)
+
+(deftest CLI-integration
+  (testing "Exit fn mocked out."
+    (is (= (exit 1 "test") {:status 1 :msg "test"})))
+  (testing "Usage generates expected text"
+    (is (string? (re-find #"search-string" (usage "search-string")))))
+  (testing "Help option returns correct exit code."
+    (is (= 0 (:status (-main "-h")))))
+  (testing "Help options returns correct help text."
+    (is (string? (re-find #"taxonomy" (:msg (-main "-h"))))))
+  (testing "Unknown option generates error status code"
+    (is (not= 0 (:status (-main "j")))))
+  (testing "Wrong number of arguments caught"
+    (is (not= 0 (:status (-main "arg1" "arg2" "spurious-arg")))))
+  (testing "Alternative filenames for input files accepted" ;; this test is passing in practice...
+    (is (= 0 (:status (-main "-t" "tax.txt" "--destinations" "dest.txt" "arg1" "arg2")))))
+  )
