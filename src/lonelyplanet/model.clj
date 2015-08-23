e53924b0d9587e97c64e4aee259e658776605972
Oliver Mooney <oliver.mooney@gmail.com>
2015-08-23 16:11:30 +0100

    Utility functions pulling key facts for each location from taxonomy.xml (model.clj)

--- src/lonelyplanet/model.clj
+++ src/lonelyplanet/model.clj
@@ -1,6 +1,7 @@
 (ns lonelyplanet.model
   (:require [clojure.data.xml :as x]
             [clojure.zip :as z]
+            [clojure.data.zip :as dz]
             [clojure.data.zip.xml :as zx]))
 
 (defn gen-parser
@@ -8,15 +9,30 @@
   [src]
   (x/parse src))
 
-;; repl capture
-(def testy (gen-parser (gen-reader "resources/test" "taxonomy.xml")))
-(def tz (clojure.zip/xml-zip testy))
-
 (defn leaves [loc]
   (filter (complement z/branch?) (take-while (complement z/end?) (iterate z/next loc))))
 
-(map #(->> % z/up z/up z/node :attrs :geo_id) (leaves tz))  ;; leaf ids
-(map #(->> % zx/text) (leaves tz))                          ;; leaf names
+(defn hierarchy
+  "Get a seq of location IDs leading to this location from a zipper loc"
+  [loc]
+  (lazy-seq (when loc (cons (-> loc z/node :attrs :geo_id) (hierarchy (z/up loc))))))
 
-;; get paths, then zip up into ref structure
+(defn placename
+  "Pull place name from zipper loc"
+  [loc]
+  (zx/text loc))
 
+(defn place-id
+  "Derive geo-id from leaf node"
+  [loc]
+  (-> loc z/up z/up z/node :attrs :geo_id))
+
+(defn location-meta
+  "Generate location meta-info from leaf node location (containing location name)"
+  [loc]
+  {:placename (placename loc)
+   :place-id (place-id loc)
+   :route (-> loc
+              z/up                                          ;; up to node-name
+              z/up                                          ;; up to node
+              hierarchy)})
--- test/lonelyplanet/model_test.clj
+++ test/lonelyplanet/model_test.clj
@@ -4,6 +4,9 @@
 
             [clojure.zip :as z]
             [clojure.data.xml :as x]
+            [clojure.data.zip :as dz]
             [clojure.data.zip.xml :as zx]))
 
 
+(def testy (gen-parser (gen-reader "resources/test" "taxonomy.xml")))
+(def tz (clojure.zip/xml-zip testy))
