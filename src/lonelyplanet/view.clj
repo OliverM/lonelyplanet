b7819ef1cad5c3daf4c984764251d47d415f08e3
Oliver Mooney <Oliver.Mooney@gmail.com>
2015-08-25 00:58:40 +0100

    Basic view generation - added css selectors to grab each of the four transformation targets in the supplied html template - added transformations to populate the above (WIP: gen-content is proof of successful targeting only, inserting default content, while the hierarchy generation needs to be reversed) - added utility function, gen-hierarchy-step, to generate intermediate navigation elements (only applied to right-hand-side navigation structure so far) - expanded utility fn in view-test namespace to use a mock location metadata structure

--- src/lonelyplanet/view.clj
+++ src/lonelyplanet/view.clj
@@ -1,7 +1,24 @@
 (ns lonelyplanet.view
   (:require [net.cgrand.enlive-html :refer :all]))
 
-(def blank "example copy.html")
+(def blank "static/example copy.html")
 
-(deftemplate destination blank [data]
-   [:title] (content "Testing"))
+(defn gen-hierarchy-step
+  "Generate <li> fragments built from destination IDs (supplied as Strings) enclosing <a> elements to their
+  corresponding pages. Assumes IDs used as page filenames."
+  [step taxonomy]
+  (when step (html [:li [:a {:href (str step ".html")} (-> (Integer. ^String step) taxonomy :placename)]])))
+
+(defn gen-content
+  "Convert destination data (supplied as a lazy xml parser) into the main HTML content for the destination page."
+  [destination]
+  (html [:h1 "Test only"] [:p "Testing, just testing."])
+  )
+
+(deftemplate destination blank [destination taxonomy]
+             [:div#header :h1] (content (get-in destination [:meta :placename]))
+             [:div.secondary-navigation :a] (content (get-in destination [:meta :placename]))
+             [:div#sidebar :div.inner] (content (html [:ul (map
+                                                             #(gen-hierarchy-step % taxonomy)
+                                                             (get-in destination [:meta :route]))]))
+             [:div#main :div.inner] (content (gen-content destination)))
--- test/lonelyplanet/view_test.clj
+++ test/lonelyplanet/view_test.clj
@@ -6,4 +6,8 @@
   "Quick utility function to create html pages from destination data, saved
   into the resources directory to pick up static/all.css"
   [data]
-  (spit "resources/test.html" (reduce str (destination data))))
+  (spit "resources/test.html" (reduce str (destination
+                                            data
+                                            {355611 {:placename "South Africa"}
+                                             355064 {:placename "Africa"}
+                                             -1 {:placename "World"}}))))
