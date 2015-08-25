396162534a19dbc6e32eb7c409176cb74396af0d
Oliver Mooney <Oliver.Mooney@gmail.com>
2015-08-25 22:48:43 +0100

    Rendering of destinations in HTML - Main destination content now rendered - converted xml tags into human-readable section titles - added utility functions to convert the depth in the content xml to the appropriate <hx> level; CSS doesn't disambiguate fantastically but presumably editing that is out-of-scope - generated main content html by converting content xml to a zipper, generating html (in hiccup format) based on the location (body copy only from leaf nodes, titling only from parent nodes)

--- src/lonelyplanet/view.clj
+++ src/lonelyplanet/view.clj
@@ -1,5 +1,8 @@
 (ns lonelyplanet.view
-  (:require [net.cgrand.enlive-html :refer :all]))
+  (:require [net.cgrand.enlive-html :refer :all]
+            [clojure.zip :as z]
+            [clojure.string :as s]
+            [aprint.core :refer [aprint]]))
 
 (def blank "static/example copy.html")
 
@@ -9,11 +12,30 @@
   [step taxonomy]
   (when step (html [:li [:a {:href (str step ".html")} (-> (Integer. ^String step) taxonomy :placename)]])))
 
+(defn make-title
+  "Convert lowercase strings with underscores instead of spaces to capitalised strings"
+  [string]
+  (-> string (s/replace "_" " ") s/capitalize))
+
+(defn depth
+  "Determine depth in content hierarchy. 1 is the highest level."
+  [loc]
+  (count (z/path loc)))
+
+(defn level-header
+  "Generate a header keyword (in Hiccup format) based on a supplied hierarchy, with 1 returning a :h1, etc."
+  [depth]
+  (keyword (str "h" depth)))
+
 (defn gen-content
   "Convert destination data (supplied as a lazy xml parser) into the main HTML content for the destination page."
   [destination]
-  (html [:h1 "Test only"] [:p "Testing, just testing."])
-  )
+  (let [destination (-> destination z/xml-zip z/down)
+        fragment-locs (take-while (complement z/end?) (iterate z/next destination))]
+    (html (map (fn [loc]
+                 (list (when (z/down loc) [(level-header (depth loc)) (-> loc z/node :tag name make-title)])
+                       (when (not (z/down loc)) [:p (-> loc z/node)])))
+               fragment-locs))))
 
 (deftemplate destination blank [destination taxonomy]
              [:div#header :h1] (content (get-in destination [:meta :placename]))
