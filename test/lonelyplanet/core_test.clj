dff2e97214b7400d8aa5c429b016987cad4a035b
Oliver Mooney <Oliver.Mooney@gmail.com>
2015-08-29 17:20:44 +0100

    Added very basic integration test to core_test.clj

--- test/lonelyplanet/core_test.clj
+++ test/lonelyplanet/core_test.clj
@@ -5,7 +5,9 @@
             [clojure.tools.cli :as cli]
             [clojure.string :as s]
             [criterium.core :as c]
-            [aprint.core :refer (aprint)]))
+            [aprint.core :refer (aprint)]
+            [lonelyplanet.model :as m]
+            [lonelyplanet.view :as v]))
 
 (defn parse-util
   "Utility testing function generating pipeline for supplied cli-options seq"
@@ -30,10 +32,20 @@
   (testing "Help option returns correct exit code."
     (is (= 0 (:status (parse-util ["-h"])))))
   (testing "Help options returns correct help text."
-    (is ("taxonomy" (re-find #"taxonomy" (:msg (parse-util ["-h"]))))))
+    (is (= "taxonomy" (re-find #"taxonomy" (:msg (parse-util ["-h"]))))))
   (testing "Unknown option generates error status code"
     (is (not= 0 (:status (parse-util ["-j"])))))
   (testing "Wrong number of arguments caught"
     (is (not= 0 (:status (parse-util ["arg1" "arg2" "spurious-arg"])))))
   (testing "Alternative filenames for input files accepted" ;; nil as correct invocation so no exit generated
     (is (= nil (:status (parse-util ["-t" "tax.txt" "--destinations" "dest.txt" "arg1" "arg2"]))))))
+
+(deftest page-generation
+  (let [{:keys [destinations dest-metas hierarchy]} (m/generate-destinations
+                                                      (gen-reader "resources/test" "taxonomy.xml")
+                                                      (gen-reader "resources/test" "destinations.xml"))]
+    (testing "Rendering example of parsed data gives expected length"
+      (is (= (count (v/render-destination (nth destinations 4) dest-metas hierarchy))
+             ;; char count since representing text in program code
+             ;; directly requires escaping of " characters, etc
+             3548)))))
